e4a97609c3a8dc80c6fd5ec9da57a80ceb64649d
dzappone <dzappone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-13 20:26:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facelift-image-replacement/trunk@59538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flir-config.php
+++ flir-config.php
@@ -19,64 +19,64 @@
 					<table>
 					<tr><td valign="top"><strong><?php _e('Unknown Font Size',"FLIR"); ?>: </strong></td><td valign="top">
 					<select name="unknown_font_size">
-					  <option value="6">6</option>
-					  <option value="8">8</option>
-					  <option value="9">9</option>
-					  <option value="10">10</option>
-					  <option value="11">11</option>
-					  <option value="12">12</option>
-					  <option value="14">14</option>
-					  <option value="16" selected="selected">16</option>
-					  <option value="18">18</option>
-					  <option value="20">20</option>
-					  <option value="24">24</option>
-					  <option value="30">30</option>
-					  <option value="36">36</option>
-					  <option value="48">48</option>
-					  <option value="60">60</option>
-					  <option value="72">72</option>
+					  <option value="6"<?php if ($flir_unknown_font_size=='6') echo ' selected="selected"'?>>6</option>
+					  <option value="8"<?php if ($flir_unknown_font_size=='8') echo ' selected="selected"'?>>8</option>
+					  <option value="9"<?php if ($flir_unknown_font_size=='9') echo ' selected="selected"'?>>9</option>
+					  <option value="10"<?php if ($flir_unknown_font_size=='10') echo ' selected="selected"'?>>10</option>
+					  <option value="11"<?php if ($flir_unknown_font_size=='11') echo ' selected="selected"'?>>11</option>
+					  <option value="12"<?php if ($flir_unknown_font_size=='12') echo ' selected="selected"'?>>12</option>
+					  <option value="14"<?php if ($flir_unknown_font_size=='14') echo ' selected="selected"'?>>14</option>
+					  <option value="16"<?php if ($flir_unknown_font_size=='16') echo ' selected="selected"'?>>16</option>
+					  <option value="18"<?php if ($flir_unknown_font_size=='18') echo ' selected="selected"'?>>18</option>
+					  <option value="20"<?php if ($flir_unknown_font_size=='20') echo ' selected="selected"'?>>20</option>
+					  <option value="24"<?php if ($flir_unknown_font_size=='24') echo ' selected="selected"'?>>24</option>
+					  <option value="30"<?php if ($flir_unknown_font_size=='30') echo ' selected="selected"'?>>30</option>
+					  <option value="36"<?php if ($flir_unknown_font_size=='36') echo ' selected="selected"'?>>36</option>
+					  <option value="48"<?php if ($flir_unknown_font_size=='48') echo ' selected="selected"'?>>48</option>
+					  <option value="60"<?php if ($flir_unknown_font_size=='60') echo ' selected="selected"'?>>60</option>
+					  <option value="72"<?php if ($flir_unknown_font_size=='72') echo ' selected="selected"'?>>72</option>
 					</select>
 						</td><td valign="top"><small><?php _e('If the font size cannot be determined automatically the size will default to this (in pixels).',"FLIR"); ?></small></td>
 					</tr>
 					<tr><td valign="top"><strong><?php _e('Cache Cleanup Frequency',"FLIR"); ?>: </strong></td><td valign="top">
 					<select name="cache_cleanup_frequency">
-					  <option value="-1" selected="selected">Never</option>
-					  <option value="1">Every time</option>
-					  <option value="2">Every other time</option>
-					  <option value="5">Every 5 times</option>
-					  <option value="10">Every 10 times</option>
-					  <option value="20">Every 20 times</option>
-					  <option value="50">Every 50 times</option>
-					  <option value="100">Every 100 times</option>
-					  <option value="250">Every 250 times</option>
-					  <option value="500">Every 500 times</option>
-					  <option value="1000">Every 1000 times</option>
-					  <option value="2500">Every 2500 times</option>
-					  <option value="5000">Every 5000 times</option>
-					  <option value="10000">Every 10000 times</option>
+					  <option value="-1"<?php if ($flir_cache_cleanup_frequency=='-1') echo ' selected="selected"'?>>Never</option>
+					  <option value="1"<?php if ($flir_cache_cleanup_frequency=='1') echo ' selected="selected"'?>>Every time</option>
+					  <option value="2"<?php if ($flir_cache_cleanup_frequency=='2') echo ' selected="selected"'?>>Every other time</option>
+					  <option value="5"<?php if ($flir_cache_cleanup_frequency=='5') echo ' selected="selected"'?>>Every 5 times</option>
+					  <option value="10"<?php if ($flir_cache_cleanup_frequency=='10') echo ' selected="selected"'?>>Every 10 times</option>
+					  <option value="20"<?php if ($flir_cache_cleanup_frequency=='20') echo ' selected="selected"'?>>Every 20 times</option>
+					  <option value="50"<?php if ($flir_cache_cleanup_frequency=='50') echo ' selected="selected"'?>>Every 50 times</option>
+					  <option value="100"<?php if ($flir_cache_cleanup_frequency=='100') echo ' selected="selected"'?>>Every 100 times</option>
+					  <option value="250"<?php if ($flir_cache_cleanup_frequency=='250') echo ' selected="selected"'?>>Every 250 times</option>
+					  <option value="500"<?php if ($flir_cache_cleanup_frequency=='500') echo ' selected="selected"'?>>Every 500 times</option>
+					  <option value="1000"<?php if ($flir_cache_cleanup_frequency=='1000') echo ' selected="selected"'?>>Every 1000 times</option>
+					  <option value="2500"<?php if ($flir_cache_cleanup_frequency=='2500') echo ' selected="selected"'?>>Every 2500 times</option>
+					  <option value="5000"<?php if ($flir_cache_cleanup_frequency=='5000') echo ' selected="selected"'?>>Every 5000 times</option>
+					  <option value="10000"<?php if ($flir_cache_cleanup_frequency=='10000') echo ' selected="selected"'?>>Every 10000 times</option>
 					</select>
 					</td><td valign="top"><small><?php _e('All generated images are cached. If you have a large site with many different bits of text being replaced, this may lead to a lot of old, outdated images in the cache.',"FLIR"); ?></small></td>
 					</tr>
 					<tr><td valign="top"><strong><?php _e('Cache Keep Time',"FLIR"); ?>: </strong></td><td valign="top">
 					  <select name="cache_keep_time">
-					    <option value="600">10 Minutes</option>
-					    <option value="3600">1 hour</option>
-					    <option value="21600">6 hours</option>
-					    <option value="43200">12 Hours</option>
-					    <option value="86400">1 Day</option>
-					    <option value="172800">2 Days</option>
-					    <option value="259200">3 Days</option>
-					    <option value="604800" selected="selected">1 Week</option>
-					    <option value="1209600">2 Weeks</option>
-					    <option value="2592000">1 Month</option>
-					    <option value="15768000">6 Months</option>
-					    <option value="31536000">1 Year</option>
+					    <option value="600"<?php if ($flir_cache_keep_time=='600') echo ' selected="selected"'?>>10 Minutes</option>
+					    <option value="3600"<?php if ($flir_cache_keep_time=='3600') echo ' selected="selected"'?>>1 hour</option>
+					    <option value="21600"<?php if ($flir_cache_keep_time=='21600') echo ' selected="selected"'?>>6 hours</option>
+					    <option value="43200"<?php if ($flir_cache_keep_time=='43200') echo ' selected="selected"'?>>12 Hours</option>
+					    <option value="86400"<?php if ($flir_cache_keep_time=='86400') echo ' selected="selected"'?>>1 Day</option>
+					    <option value="172800"<?php if ($flir_cache_keep_time=='172800') echo ' selected="selected"'?>>2 Days</option>
+					    <option value="259200"<?php if ($flir_cache_keep_time=='259200') echo ' selected="selected"'?>>3 Days</option>
+					    <option value="604800"<?php if ($flir_cache_keep_time=='604800') echo ' selected="selected"'?>>1 Week</option>
+					    <option value="1209600"<?php if ($flir_cache_keep_time=='1209600') echo ' selected="selected"'?>>2 Weeks</option>
+					    <option value="2592000"<?php if ($flir_cache_keep_time=='2592000') echo ' selected="selected"'?>>1 Month</option>
+					    <option value="15768000"<?php if ($flir_cache_keep_time=='15768000') echo ' selected="selected"'?>>6 Months</option>
+					    <option value="31536000"<?php if ($flir_cache_keep_time=='31536000') echo ' selected="selected"'?>>1 Year</option>
 					  </select>
 					</td><td valign="top"><small><?php _e('When the cache cleanup runs, cached images that are older than the time provided here will be deleted (Unix timestamp).  This value is not used if Cache Cleanup Frequency is disabled.',"FLIR"); ?></small></td>
 					</tr>
 					<tr>
 						<td valign="top"><strong><?php _e('Horizontal Text Bounds',"FLIR"); ?>: </strong></td><td valign="top">
-					  	<input name="horizontal_text_bounds" type="text" value="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" size="60" maxlength="254" />
+					  	<input name="horizontal_text_bounds" type="text" value="<?php if (empty($flir_horizontal_text_bounds)) { echo 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; } else {echo $flir_horizontal_text_bounds;}?>" size="60" maxlength="254" />
 					  </td><td valign="top"><small><?php _e('This will only be used if some fonts have characters that will extend below the baseline. For example, p, q, or y all have tails that extend below the baseline.  The text will be used to attempt to figure out the lowest and highest point of all the characters. This will create a uniform height across all generated images for a particular font size.  You should not have to change this value unless you are working in a language that does not use the a-z alphabet.',"FLIR"); ?></small></td>
 					</tr>
 <!--					<tr><td valign="top"><strong><?php// _e('Font Discovery',"FLIR"); ?>: </strong></td>
@@ -86,10 +86,9 @@
 					</tr>  -->
 					<tr><td valign="top"><strong><?php _e('JavaScript Method',"FLIR"); ?>: </strong></td><td valign="top">
 						<select name="javascript_method">
-					    <option value="native">Native</option>
-					    <option value="jquery" selected="selected">jQuery</option>
-					    <option value="prototype">Prototype</option>
-					    <option value="scriptalicious">Scriptalicious</option>
+					    <option value="jquery"<?php if ($flir_javascript_method=='jquery') echo ' selected="selected"'?>>jQuery</option>
+					    <option value="prototype"<?php if ($flir_javascript_method=='prototype') echo ' selected="selected"'?>>Prototype</option>
+					    <option value="scriptalicious"<?php if ($flir_javascript_method=='scriptalicious') echo ' selected="selected"'?>>Scriptalicious</option>
 					  </select>
 						</td><td valign="top"><small><?php _e('Choose a JavaScript library to assist in the rendering.  jQuery seems to be the fastest but you may alreay be loading a particular library and prefer that one.',"FLIR"); ?></small></td>
 					</tr>
@@ -110,19 +109,19 @@
 					?>
 					  <tr><td valign="top"><strong><?php _e('Fonts List',"FLIR"); ?>: </strong></td><td valign="top">
 					  <select name="fonts_list[]" size="8" multiple="multiple" style="height:120px;">
-					  <?php if (!empty($fonts_list)) {foreach ( $fonts_list as $key=>$value) {echo '<option ';if ($flir_fonts_list['comt'] == $key) echo 'selected="selected" '; echo 'value="'.$key.'">'.ucfirst($key).' ('.$value.')</option>';}}?>
+					  <?php if (!empty($fonts_list)) {foreach ( $fonts_list as $key=>$value) {echo '<option ';if ($this->setSelected($value,$flir_fonts_list)) echo 'selected="selected" '; echo 'value="'.$value.'">'.ucfirst($key).' ('.$value.')</option>'.$this->eol;}}?>
 					  </select>
 						</td><td valign="top"><small><?php _e('All the fonts you have in the fonts directory should be listed here.  Use CTRL+Click to choose which fonts to use.',"FLIR"); ?></small></td>
 					</tr>
 					  <tr><td valign="top"><strong><?php _e('Default Font',"FLIR"); ?>: </strong></td><td valign="top">
 					  <select name="font_default">
-						<?php if (!empty($fonts_list)) {foreach ( $fonts_list as $key=>$value) {echo '<option ';/*if ($flir_elements_fonts[7]==$key) echo 'selected="selected" '; */echo 'value="'.$key.'">'.ucfirst($key).' ('.$value.')</option>';}}?>
+						<?php if (!empty($fonts_list)) {foreach ( $fonts_list as $key=>$value) {echo '<option ';if ($flir_font_default==$value) echo 'selected="selected" '; echo 'value="'.$value.'">'.ucfirst($key).' ('.$value.')</option>'.$this->eol;}}?>
 						</select>
 						</td><td valign="top"><small><?php _e('If the font cannot be determined automatically the font will default to this.',"FLIR"); ?></small></td>
 					</tr>
 					<tr>
 						<td valign="top"><strong><?php _e('ImageMagick Path',"FLIR"); ?>: </strong></td><td valign="top">
-					  	<input name="imagemagick_path" type="text" value="/usr/bin/" size="60" maxlength="254" />
+					  	<input name="imagemagick_path" type="text" value="<?php if (empty($flir_horizontal_text_bounds)) { echo '/usr/bin/'; } else {echo $flir_imagemagick_path;}?>" size="60" maxlength="254" />
 					  </td><td valign="top"><small><?php _e('Set this to the location of your ImageMagick binaries (with a trailing slash).  Required only if you are using Facelift plugins',"FLIR"); ?></small></td>
 					</tr>
 					</table>
@@ -133,17 +132,5 @@
 			</div>
 			</div>
 <?php
-function select_font_list($thefont,$fontlist) {
-	if (!empty($fontlist)) {
-		foreach ($fontlist as $key=>$value) {
-			if ($key == $thefont) {
-				 $ret_value = 'selected="selected"';
-			}
-			else {
-				 $ret_value = '';
-			}
-		}
-	}
-	return $ret_value;
-}		
+
 ?>
--- flir.php
+++ flir.php
@@ -4,13 +4,14 @@ Plugin Name: FLIR for WordPress
 Plugin URI: http://www.23systems.net/plugins/facelift-image-replacement-flir/
 Description: Facelift Image Replacment for WordPress is a plugin and script is a script that generates image representations of text on your web page in fonts that visitors would not be able to see.  It is based on Facelift Image Replacement by <a href="http://facelift.mawhorter.net/">Cory Mawhorter</a>.
 Author: Dan Zappone
-Version: 0.5.7
+Version: 0.5.9
 Author URI: http://www.23systems.net/
 */
-global $flir_path, $facelift_path, $flir_fonts, $fonts;
+global $flir_path, $facelift_path, $flir_fonts, $fonts, $facelift_config;
 require('facelift/config-flir.php');
 $flir_path     = WP_PLUGIN_URL.'/facelift-image-replacement';
 $facelift_path = $flir_path.'/facelift/';
+$facelift_config = dirname(__FILE__)."/facelift/config-flir.php";
 $flir_fonts    = $fonts;
 if (!class_exists('wp_flir')) {
 
@@ -50,6 +51,14 @@ if (!class_exists('wp_flir')) {
 
     /*---- Saves the admin options to the database. ----*/
     function saveAdminOptions($optionsname, $options) {
+      if ($optionsname == 'wp_flir_options') {
+        echo '<div id="message" class="updated fade">';
+		    _e('<p>FLIR Display Options Updated</p></div>',"FLIR");
+		  }
+      elseif ($optionsname == 'wp_flir_config') {
+        echo '<div id="message" class="updated fade">';
+		    _e('<p>FLIR Configuration Updated</p></div>',"FLIR");
+      }
       update_option($optionsname, $options);
     }
 
@@ -99,6 +108,46 @@ if (!class_exists('wp_flir')) {
             "imagemagick_path"        => $flir_imagemagick_path,
           );
           $this->saveAdminOptions($this->adminConfigName, $flir_config_options);
+
+          $flir_config_data = '';
+          $flir_config_data .= "<?php".$this->eol();
+          $flir_config_data .= "/* Facelift Image Replacement v1.1.1".$this->eol().$this->eol();
+
+          $flir_config_data .= "Facelift was written and is maintained by Cory Mawhorter.".$this->eol();
+          $flir_config_data .= "It is available from http://facelift.mawhorter.net/  */".$this->eol().$this->eol();
+          
+          $flir_config_data .= "define('UNKNOWN_FONT_SIZE', ".$flir_unknown_font_size."); // in pixels".$this->eol().$this->eol();
+
+          $flir_config_data .= "define('CACHE_CLEANUP_FREQ', ".$flir_cache_cleanup_frequency."); // -1 disable, 1 everytime, 10 would be about 1 in 10 times this script runs (higher number decreases frequency)".$this->eol();
+          $flir_config_data .= "define('CACHE_KEEP_TIME', ".$flir_cache_keep_time."); // 604800: 7 days".$this->eol();
+          $flir_config_data .= "define('CACHE_DIR', 'cache');".$this->eol();
+          $flir_config_data .= "define('FONTS_DIR', 'fonts');".$this->eol();
+          $flir_config_data .= "define('PLUGIN_DIR', 'plugins');".$this->eol().$this->eol();
+
+          $flir_config_data .= "define('HBOUNDS_TEXT', '".$flir_horizontal_text_bounds."'); // see http://facelift.mawhorter.net/docs/".$this->eol().$this->eol();
+
+          $flir_config_data .= "define('FONT_DISCOVERY', false);".$this->eol().$this->eol();
+
+          $flir_config_data .= "define('IM_EXEC_PATH', '".$flir_imagemagick_path."');  // ImageMagick binaries path".$this->eol().$this->eol();
+
+          $flir_config_data .= "// Each font you want to use should have an entry in the fonts array.".$this->eol();
+          $flir_config_data .= '$fonts = array();'.$this->eol();
+
+          if ($flir_fonts_list){
+            foreach ($flir_fonts_list as $key=>$value){
+              $flir_config_data .= '$fonts[\''.$this->keymaker($value).'\'] 	= \''.$value.'\';'.$this->eol();
+              }
+          }
+          $flir_config_data .= $this->eol().'// The font will default to the following (put your most common font here).'.$this->eol();
+          $flir_config_data .= '$fonts[\'default\'] 	= \''.$flir_font_default.'\';'.$this->eol().$this->eol();
+          
+          $flir_config_data .= '// Set replacement for "web fonts" here'.$this->eol();
+          $flir_config_data .= '//$fonts[\'arial\'] = $fonts[\'helvetica\'] = $fonts[\'sans-serif\'] 		= $fonts[\'puritan\'];'.$this->eol();
+          $flir_config_data .= '//$fonts[\'times new roman\'] = $fonts[\'times\'] = $fonts[\'serif\'] 		= $fonts[\'bentham\'];'.$this->eol();
+          $flir_config_data .= '//$fonts[\'courier new\'] = $fonts[\'courier\'] = $fonts[\'monospace\'] 	= $fonts[\'geo\'];'.$this->eol();
+          $flir_config_data .= '?'.'>'.$this->eol();
+          
+          $this->writeConfig($flir_config_data);
 
           /*	        echo "unknown_font_size: ".$unknown_font_size."<br />";
                     echo "cache_cleanup_frequency: ".$cache_cleanup_frequency."<br />";
@@ -179,6 +228,7 @@ require('flir-config.php');
       echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('wpurl').'/wp-content/plugins/facelift-image-replacement/css/admin.css" />'.$this->eol();
     }
 
+    /*---- UTILITY FUNCTIONS ----*/
     /*---- Create clean eols for source ----*/
     function eol() {
       if (strtoupper(substr(PHP_OS, 0, 3) == 'WIN')) {
@@ -192,6 +242,33 @@ require('flir-config.php');
       }
       return $eol;
     }
+    
+    /*---- sets selected array items in multi select form field to selected ----*/
+    function setSelected($theitem,$thearray) {
+      $ret_value = false;
+	    if (!empty($thearray)) {
+		    foreach ($thearray as $key=>$value) {
+			   if ($value == $theitem) { $ret_value = true; }
+		    }
+	    }
+	   return $ret_value;
+    }
+    
+    /*---- Reduce filename to acceptable array key ----*/
+    function keymaker($filename) {
+      $replacepattern = "/(\.otf)|(\.ttf)|(-*_*\d*\s*)/i";
+      $keyname = strtolower(preg_replace($replacepattern, '', $filename));
+      return $keyname;
+    }
+    
+    /*---- Writes facelift config file to facelift directory ----*/
+    function writeConfig($configdata) {
+      global $facelift_config;
+      $handle = fopen($facelift_config, 'wb');
+      fwrite($handle, $configdata);
+      fclose($handle);
+    }
+
   }
 }
 
