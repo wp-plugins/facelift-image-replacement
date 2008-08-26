4063e8f9aebe52b2f7b09da5c7162afb1cc5a622
dzappone <dzappone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-26 19:26:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facelift-image-replacement/trunk@61730 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facelift/plugins/QuickEffects.php
+++ facelift/plugins/QuickEffects.php
@@ -2,7 +2,7 @@
 // JavaScript Document
 
 /*
-QuickEffects v0.1.1
+QuickEffects v0.2
 
 Facelift was written and is maintained by Cory Mawhorter.  
 It is available from http://facelift.mawhorter.net/
@@ -25,91 +25,172 @@ You should have received a copy of the GNU General Public License
 along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+function add_girth($wh, $bHeight=false) {
+	global $add_height,$add_width;
+	
+	if($bHeight) 
+		if($wh>$add_height)
+			$add_height = $wh;
+	else
+		if($wh>$add_width)
+			$add_width = $wh;
+}
+
 
 $PLUGIN_ERROR = false;
-define('FULL_CACHE_PATH', getcwd().'/'.$cache);
+define('FULL_CACHE_PATH', fix_path(getcwd().'/'.$FLIR['cache']));
+define('QE_DIR', fix_path(getcwd().'/'.PLUGIN_DIR.'/QuickEffects'));
+define('CONVERT', IM_EXEC_PATH.'convert');
+	
+if(!file_exists(QE_DIR)) {
+	@mkdir(QE_DIR);
+	if(!file_exists(QE_DIR))
+		die('QuickEffects directory does not exists and cannot be created.');
+}
+
+if(DEBUG && file_exists(FULL_CACHE_PATH))
+	unlink(FULL_CACHE_PATH);
 
 $image = false;
 
-if($text[0] == '@') $text = '\\'.$text;
+if($FLIR['text'][0] == '@') $FLIR['text'] = '\\'.$FLIR['text'];
+$cmdtext = escapeshellarg($FLIR['text']);
 
-$bounds = convertBoundingBox(imagettfbbox($size, 0, $font, $text));
+$bounds = convertBoundingBox(imagettfbbox($FLIR['size_pts'], 0, $FLIR['font'], $FLIR['text']));
+
 $fulltrim = '';
 if($FStyle['realFontHeight']!='true') {
 	$bounds['height'] += 200;
 	$REAL_HEIGHT_BOUNDS = $bounds;	
-	$fulltrim = '-trim +repage';
+	$fulltrim = '-trim +repage ';
 }
 	
-$fore_hex = dec2hex($color['red'], $color['green'], $color['blue']);
-$bkg_hex = dec2hex(abs($color['red']-100), abs($color['green']-100), abs($color['blue']-100));
-$dpi = preg_match('#^[0-9]+$#', $FStyle['dpi']) ? $FStyle['dpi'] : 96;
-$size = $size + ($size-((72/$dpi)*$size));
-$out_width  = $bounds['width']+500;
+$fore_hex = dec2hex($FLIR['color']['red'], $FLIR['color']['green'], $FLIR['color']['blue']);
+$bkg_hex = dec2hex(abs($FLIR['color']['red']-100), abs($FLIR['color']['green']-100), abs($FLIR['color']['blue']-100));
+$out_width  = $bounds['width']+300;
 $out_height = $REAL_HEIGHT_BOUNDS['height'];
 
+$opacity = '';
+if($FLIR['opacity'] < 100 && $FLIR['opacity'] >= 0)
+	$opacity = strlen($FLIR['opacity']) == 1 ? '0'.$FLIR['opacity'] : (strlen($FLIR['opacity'])>2?substr($FLIR['opacity'], 0, 2) : $FLIR['opacity']);
+
+$add_width = $add_height = 0;
+
 $stroke = '';
+$strokwidth = 0;
 if(isset($FStyle['qe_Stroke'])) {
-	list($strokewidth, $strokecolor) = explode(',', $FStyle['qe_Stroke'], 2);
+	list($strokewidth, $strokecolor, $quality) = explode(',', $FStyle['qe_Stroke'], 3);
+	
 	$strokewidth=trim($strokewidth);
 	$strokecolor=trim($strokecolor);
-	$stroke = '-strokewidth '.(is_number($strokewidth)?$strokewidth:1).' -stroke "#'.(is_hexcolor($strokecolor)?strtoupper($strokecolor):'FF0000').'" ';
+	$strokewidth = is_number($strokewidth)?$strokewidth:1;
+	$strokecolor = escapeshellarg('#'.(is_hexcolor($strokecolor)?strtoupper($strokecolor):'FF0000'));
+
+	if(trim($quality) == 'high') {
+		for($i=1; $i <= $strokewidth; $i++) {
+			$stroke .= '-fill '.$strokecolor.' -annotate 0x0-'.$i.'-'.$i.' '.$cmdtext.' ';
+			$stroke .= '-fill '.$strokecolor.' -annotate 0x0+'.$i.'-'.$i.' '.$cmdtext.' ';
+			$stroke .= '-fill '.$strokecolor.' -annotate 0x0+'.$i.'+'.$i.' '.$cmdtext.' ';
+			$stroke .= '-fill '.$strokecolor.' -annotate 0x0-'.$i.'+'.$i.' '.$cmdtext.' ';
+		}
+	}else {
+		$stroke = '-strokewidth '.$strokwidth.' -stroke '.$strokecolor;
+	}
 	
-	$out_width+= ($strokewidth*2);
-	$out_height+= ($strokewidth*2);
+	add_girth($strokewidth*2);
+	add_girth($strokewidth*2, true);
 }
 
+$fill = '-fill '.escapeshellarg('#'.$fore_hex.$opacity).' ';
 if(isset($FStyle['qe_Fill'])) {
 	list($fill_type, $fill_options) = explode(',', $FStyle['qe_Fill'], 2);
 	switch($fill_type) {
 		case 'pattern':
-			$cmds = array(IM_EXEC_PATH.'convert -size '.$out_width.'x'.$out_height.' -channel RGBA xc:"#'.$bkg_hex.'00" -font "'.$font.'" -pointsize '.$size.' -tile "'.getcwd().'/'.$fill_options.'" -gravity Center '.$stroke.' -annotate 0 "'.addcslashes($text, '"').'" '.$fulltrim.' "'.FULL_CACHE_PATH.'"');
+			$pattern_file = QE_DIR.'/'.basename($fill_options);
+			if(!file_exists($pattern_file))
+				die('Pattern file does not exists.  Be sure that you have uploaded the pattern file to the QuickEffects directory.');
+			$fill = '-tile '.escapeshellarg($pattern_file).' ';
 			break;
 		default:
 		case 'gradient':
 			list($color1, $color2) = explode(',', $fill_options, 2);
 			$color1 = is_hexcolor($color1)?strtoupper($color1):'999999';
 			$color2 = is_hexcolor($color2)?strtoupper($color2):'000000';
-			$cmds = array(IM_EXEC_PATH.'convert -size '.$out_width.'x'.$out_height.' -channel RGBA xc:"#'.$bkg_hex.'00" -font "'.$font.'" -pointsize '.$size.' -tile gradient:"#'.$color1.'"-"#'.$color2.'" -gravity Center '.$stroke.' -annotate 0 "'.addcslashes($text, '"').'" '.$fulltrim.' "'.FULL_CACHE_PATH.'"');
+			$fill = '-tile gradient:'.escapeshellarg('#'.$color1.$opacity).'-'.escapeshellarg('#'.$color2.$opacity).' ';
 			break;
 	}
-}else {
-	$cmds = array(IM_EXEC_PATH.'convert -size '.$out_width.'x'.$out_height.' -background "#'.$bkg_hex.'00" -channel RGBA -fill "#'.$fore_hex.'" -font "'.$font.'" -pointsize '.$size.' -gravity Center '.$stroke.' caption:"'.addcslashes($text, '"').'" '.$fulltrim.' "'.FULL_CACHE_PATH.'"');
+}
+
+$extrude = '';
+if(isset($FStyle['qe_Extrude'])) {
+	list($extrudedirection, $extrudewidth, $extrudecolor) = explode(',', $FStyle['qe_Extrude'], 3);
+	
+	
+	$extrudewidth=trim($extrudewidth);
+	$extrudecolor=trim($extrudecolor);
+	$extrudewidth = is_number($extrudewidth)?$extrudewidth:1;
+	$extrudecolor = escapeshellarg('#'.(is_hexcolor($extrudecolor)?strtoupper($extrudecolor):'FF0000').$opacity);
+
+	add_girth($width*2);
+	add_girth($width*2, true);
+	
+	$offset = $strokewidth;
+	
+	for($i=$strokewidth; $i < ($extrudewidth+$strokewidth); $i++) {
+		$extrude .= '-fill '.$color.' -annotate 0x0';
+		switch($extrudedirection) {
+			case 'ne':
+				$extrude .= '+'.$i.'-'.$i;
+				break;
+			case 'nw':
+				$extrude .= '-'.$i.'-'.$i;
+				break;
+			default:
+			case 'se':
+				$extrude .= '+'.$i.'+'.$i;
+				break;
+			case 'sw':
+				$extrude .= '-'.$i.'+'.$i;
+				break;		
+		}
+		$extrude .= ' '.$cmdtext.' ';
+	}
 }
 
 // SHADOW
+$shadow = '';
 if(isset($FStyle['qe_Shadow'])) {
 	switch($FStyle['qe_Shadow']) {
 		case 'high':
 			$shadow = array('opacity' 		=> 55
 								,'sigma' 		=> 3
-								,'offset-left' => '+2'
-								,'offset-top' 	=> '+2');
+								,'left' => '+2'
+								,'top' 	=> '+2');
 			break;
 		case 'low':
 			$shadow = array('opacity' 		=> 65
 								,'sigma' 		=> 2
-								,'offset-left' => '+2'
-								,'offset-top' 	=> '+2');
+								,'left' => '+2'
+								,'top' 	=> '+2');
 			break;
 		case 'fuzzy':
 			$shadow = array('opacity' 		=> 55
 								,'sigma' 		=> 4
-								,'offset-left' => '+0'
-								,'offset-top' 	=> '+0');
+								,'left' => '+0'
+								,'top' 	=> '+0');
 			break;
-		/*
 		case 'hard':
-			$shadow = array('opacity' 		=> 75
+			$shadow = array('opacity' 		=> 90
 								,'sigma' 		=> 0
-								,'offset-left' => '+1'
-								,'offset-top' 	=> '+1');
+								,'left' => '+1'
+								,'top' 	=> '+1');
 			break;
+/*
 		case 'perspective':
 			$shadow = array('opacity' 		=> 75
 								,'sigma' 		=> 0
-								,'offset-left' => '+1'
-								,'offset-top' 	=> '+1');
+								,'left' => '+1'
+								,'top' 	=> '+1');
 			break;
 		*/
 		
@@ -119,31 +200,39 @@ if(isset($FStyle['qe_Shadow'])) {
 			$shadow_sig = is_number($shadow_sig)?$shadow_sig:2;
 			$shadow_ol = preg_match('#^[+-][0-9]{1,4}$#', $shadow_ol)?$shadow_ol:'+2';
 			$shadow_ot = preg_match('#^[+-][0-9]{1,4}$#', $shadow_ot)?$shadow_ot:'+2';
-			$shadow = array('opacity' 		=> $shadow_opac
-								,'sigma' 		=> $shadow_sig
-								,'offset-left' => $shadow_ol
-								,'offset-top' 	=> $shadow_ot);
+			$shadow = array('opacity' 	=> $shadow_opac
+								,'sigma' 	=> $shadow_sig
+								,'left' 		=> $shadow_ol
+								,'top' 		=> $shadow_ot);
 			break;
 	}
-						
-	$cmds[] = IM_EXEC_PATH.'convert "'.FULL_CACHE_PATH.'" -matte ( +clone -background "#'.bkg_hex.'" -shadow '.$shadow['opacity'].'x'.$shadow['sigma'].$shadow['offset-left'].'x-5 ) +swap -background none -mosaic "'.FULL_CACHE_PATH.'"';
+	
+	$shadow = (IS_WINDOWS?'':'\\').'( +clone -background black  -shadow '.$shadow['opacity'].'x'.$shadow['sigma'].$shadow['left'].$shadow['top'].' '.(IS_WINDOWS?'':'\\').') +swap -background none ';
+	
+	$left = substr($shadow['offset-left'], 1);
+	$top = substr($shadow['offset-top'], 1);
+	
+	if($left>0)
+		add_girth($left*2);
+	if($top>0)
+		add_girth($top*2, true);
 }
 
-/*
-header('Content-encoding: utf-8');
-header('Content-type: text/plain');
-foreach($cmds as $cmd)
-	echo $cmd."\n";
-exit;
-*/
+$out_width 	+= $add_width;
+$out_height += $add_height;
 
-$cmds[] = IM_EXEC_PATH.'convert "'.FULL_CACHE_PATH.'" -matte -background none -flatten '.$fulltrim.' "'.FULL_CACHE_PATH.'"';
+$cmd = CONVERT.' -size '.$out_width.'x'.$out_height.' xc:transparent '
+					.'-font '.escapeshellarg(fix_path($FLIR['font']))
+					.' -density '.$FLIR['dpi'].' -pointsize '.$FLIR['size_pts'].' -gravity Center '
+					.$extrude.' '
+					.$stroke.' '
+					.$fill.' '
+					.' -annotate 0 '.$cmdtext.' '
+					.$shadow.' '
+					.' -mosaic -flatten '.$fulltrim.' '.escapeshellarg(FULL_CACHE_PATH);
 
-
-foreach($cmds as $cmd)
-	exec($cmd);
-
-
+//die($cmd);
+exec($cmd);
 
 if($FStyle['realFontHeight']=='true') { // trim sides
 	/*
@@ -155,10 +244,11 @@ if($FStyle['realFontHeight']=='true') { // trim sides
 		 [5] => +4
 		 [6] => +32
 	*/
-	$info = shell_exec(IM_EXEC_PATH.'convert "'.FULL_CACHE_PATH.'" -trim info:');
-	preg_match('#PNG ([0-9]+)x([0-9]+) ([0-9]+)x([0-9]+)([+-][0-9]+)([+-][0-9]+)#', $info, $m);
-//	print_r($m);
-	exec(IM_EXEC_PATH.'convert "'.FULL_CACHE_PATH.'" -crop '.$m[1].'x'.$m[4].$m[5].'+0 +repage "'.FULL_CACHE_PATH.'"');
+	$info = shell_exec(CONVERT.' '.escapeshellarg(FULL_CACHE_PATH).' -trim info:');
+	if(preg_match('#PNG ([0-9]+)x([0-9]+) ([0-9]+)x([0-9]+)([+-][0-9]+)([+-][0-9]+)#', $info, $m))
+		exec(CONVERT.' '.escapeshellarg(FULL_CACHE_PATH)
+				.' -crop '.$m[1].'x'.$m[4].$m[5].'+0 +repage '
+				.escapeshellarg(FULL_CACHE_PATH));
 }
 
 ?>
